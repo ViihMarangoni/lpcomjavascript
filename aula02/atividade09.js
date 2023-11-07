354adf19b8ee21162d991286942b2f795c57ef06
ViihMarangoni <vmarangoni113@icloud.com>
2023-11-07 19:15:09 -0300

    09-10

--- aula02/atividade09.js
+++ aula02/atividade09.js
@@ -1,1 +1,12 @@
 // Faça um Programa que peça um número inteiro e determine se ele é par ou impar. Dica: utilize o operador módulo (resto da divisão).
+
+let numero = 3
+
+console.log("Digite um número inteiro: ")
+
+if(numero % 2 == 0){
+    console.log("Par")
+}else{
+    console.log("Impar")
+}
+    
